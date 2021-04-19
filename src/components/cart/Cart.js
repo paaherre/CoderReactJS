dde47fb9dfc47def72a669f0411827b01c8ceb7e
Pablo Herrera <paaherre@gmail.com>
2021-04-19 16:49:33 -0300

    updateCartView

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../Context/CartContext';
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 import "./Cart.css"
 
 
 const Cart = () => {
-    const { cart, addItem, removeItem, clear, totalCant, totalPrec } = useContext(CartContext)
+    const { cart, removeItem, clear, totalCant, totalPrec } = useContext(CartContext)
 
 
 
@@ -15,21 +17,32 @@ const Cart = () => {
                 !cart.length ?
                     <h2> No hay items en el carrito <Link to='/'> Ir al Home</Link></h2>
                     : (<>
-                        {cart.map(cartItem =>
-                            <div key={cartItem.item.id}>
-                                <div> Producto: {cartItem.item.nombre} </div>
-                                <div> Precio: {cartItem.item.precio}</div>
-                                <div> Cantidad: {cartItem.cantidad} </div>
-                                <div> Subtotal: {cartItem.cantidad * cartItem.item.precio}</div>
-                                <button onClick={() => removeItem(cartItem.item.id)}> Quitar </button>
-                            </div>
-
-                        )}
-
-
-                        <div>Cantidad de Productos: {totalCant}</div>
-                        <div>Precio Productos: {totalPrec}</div>
-                        <button onClick={() => clear()}>Vaciar Carrito</button>
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                    <th>Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cart.map(cartItem =>
+                                    <tr key={cartItem.item.id}>
+                                        <td> {cartItem.item.nombre} </td>
+                                        <td> {cartItem.item.precio}</td>
+                                        <td> {cartItem.cantidad} </td>
+                                        <td> {cartItem.cantidad * cartItem.item.precio}</td>
+                                        <td><Button variant="danger" onClick={() => removeItem(cartItem.item.id)}> Quitar </Button></td>
+                                    </tr>
+                                )}
+                                <tr>
+                                    <td colSpan="2">Cantidad de Productos: {totalCant}</td>
+                                    <td colSpan="2">TOTAL: {totalPrec}</td>
+                                    <td><Button variant="danger" onClick={() => clear()}>Vaciar Carrito</Button></td>
+                                </tr>
+                            </tbody>
+                        </Table>
                     </>
                     )
             }
