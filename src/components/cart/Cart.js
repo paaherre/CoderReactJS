f5020baf444135966be550a440765f1d9361bbea
Pablo Herrera <paaherre@gmail.com>
2021-04-27 00:55:58 -0300

    updates Buyer FB2

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,20 +4,50 @@ import { CartContext } from '../../Context/CartContext';
 import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import "./Cart.css"
+import { getFirestore } from '../../firebase';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 
 const generarOrden = (cart, totalPrec) => {
+
+    const db = getFirestore();
+    const ordenCol = db.collection('ordenes');
+
     let orden = {}
 
+    orden.date = firebase.firestore.Timestamp.fromDate(new Date());
     orden.buyer = { name: 'juan', phone: 'my phone', email: 'pepe@aol.com.ar' }
     orden.total = totalPrec
     orden.items = cart.map(cartItem => {
         const id = cartItem.item.id;
         const nombre = cartItem.item.nombre;
+        const cantidad = cartItem.cantidad
         const precio = cartItem.item.precio * cartItem.cantidad;
 
-        return { id, nombre, precio }
+        return { id, nombre, cantidad, precio }
     })
+
+
+
+    ordenCol.add(orden)
+        .then((IdDocumento) => {
+            console.log(IdDocumento.id)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            console.log('termino la promesa')
+        })
+
+    for (const cartItem of cart) {
+        const docRef = db.collection('productos').doc(cartItem.item.id)
+
+        docRef.update({
+            stock: cartItem.item.stock - cartItem.cantidad
+        })
+    }
 
     console.log(orden);
 }
